e9080e4a122439fca262f84fccb6e278f57a3d06
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-01-16 12:00:31 +1100

    featured products card component completed (home page)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./ui/hero";
+import CategoryCards from "./ui/categoryCards";
+import FeaturedProjects from "./ui/featuredProjects";
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <CategoryCards />
+      <FeaturedProjects />
     </main>
   );
 }
---
+++ src/app/ui/featuredProjects.jsx
@@ -0,0 +1,90 @@
+import styles from "@/app/styles/featuredProducts.module.css";
+import ButtonLink from "./buttonLink";
+export default function FeaturedProjects() {
+  return (
+    <section className={styles.featuredProducts}>
+      <div className={styles.cardOne}>
+        <div className={styles.cardOneImgContainer}>
+          <img
+            className={styles.circlesPattern}
+            src="/assets/home/desktop/pattern-circles.svg"
+            alt=""
+          />
+          <picture>
+            <source
+              srcSet="/assets/home/desktop/image-speaker-zx9.png"
+              media="(min-width: 1024px) "
+            />
+            <source
+              srcSet="/assets/home/tablet/image-speaker-zx9.png"
+              media="(min-width: 768px) "
+            />
+            <img
+              className={styles.cardOneImg}
+              src="/assets/home/mobile/image-speaker-zx9.png"
+              alt="The zx9 speaker"
+            />
+          </picture>
+        </div>
+        <div className={styles.cardOneContent}>
+          <h2 className={styles.cardOneTitle}>
+            zx9 <br /> speaker
+          </h2>
+          <div className={styles.cardOneDescription}>
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </div>
+          <ButtonLink url="/product/1" theme="secondary2">
+            See product
+          </ButtonLink>
+        </div>
+      </div>
+      <div className={styles.cardTwo}>
+        <picture>
+          <source
+            srcSet="/assets/home/desktop/image-speaker-zx7.jpg"
+            media="(min-width: 1024px)"
+          />
+          <source
+            srcSet="/assets/home/tablet/image-speaker-zx7.jpg"
+            media="(min-width: 500px)"
+          />
+          <img
+            src="/assets/home/mobile/image-speaker-zx7.jpg"
+            alt="The zx7 speaker"
+            className={styles.cardTwoImg}
+          />
+        </picture>
+        <div className={styles.cardTwoContent}>
+          <h2 className={styles.cardTwoTitle}>zx7 speaker</h2>
+          <ButtonLink url="/product/1" theme="secondary">
+            See Product
+          </ButtonLink>
+        </div>
+      </div>
+      <div className={styles.cardThree}>
+        <picture>
+          <source
+            srcSet="/assets/home/desktop/image-earphones-yx1.jpg"
+            media="(min-width: 1024px)"
+          />
+          <source
+            srcSet="/assets/home/tablet/image-earphones-yx1.jpg"
+            media="(min-width: 768px)"
+          />
+          <img
+            src="/assets/home/mobile/image-earphones-yx1.jpg"
+            alt="The yx1 earphones"
+            className={styles.cardThreeImg}
+          />
+        </picture>
+        <div className={styles.cardThreeContent}>
+          <h2 className={styles.cardThreeTitle}>yx1 earphones</h2>
+          <ButtonLink url="/product/1" theme="secondary">
+            See product
+          </ButtonLink>
+        </div>
+      </div>
+    </section>
+  );
+}
